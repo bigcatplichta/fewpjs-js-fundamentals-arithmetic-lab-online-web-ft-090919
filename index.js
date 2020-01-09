38fb9b2fc4387ddf7eb6f5b33fa490dc673ec443
Matt Plichta <bigcatplichta@gmail.com>
2020-01-09 22:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let num1 = 31
 let num2 = 2
 
-const multiply = num1 * num2
+const multiply = num1 * num2;
 
-const random = 
+const random = Math.random();
 
 // apparently the tests don't want functions?
 // function multiply(num1, num2) {
