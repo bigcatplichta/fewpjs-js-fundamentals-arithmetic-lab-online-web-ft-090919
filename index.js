3e4e79b9943a8f15f3230da58cd3064d2e1bcca3
Matt Plichta <bigcatplichta@gmail.com>
2020-01-09 22:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+let num1 = 31
+let num2 = 2
 
+const multiply = num1 * num2
 
 // apparently the tests don't want functions?
-function multiply(num1, num2) {
-  num1 * num2
-}
+// function multiply(num1, num2) {
+//   num1 * num2
+// }
