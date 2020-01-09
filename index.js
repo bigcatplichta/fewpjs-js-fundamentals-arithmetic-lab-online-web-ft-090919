ebc367c2daa624179d1f4eef411f0579c8048ad7
Matt Plichta <bigcatplichta@gmail.com>
2020-01-09 22:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let num1 = 31
-let num2 = 2
+let num1 = 16
+let num2 = 4
 
-let multiply = num1 * num2;
+let multiply = num1 * num2
 
 // return number between 0 and 100
-let random = Math.round(Math.random() * 100);
+let random = Math.round(Math.random() * 100)
 
 let mod = 25 % 7
+
+let max = Math.max(num1, num2, (random - 100), 20)
 
 // apparently the tests don't want functions?
 
