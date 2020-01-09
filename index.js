a025f8004fc1d647dac6af9b527fa2a09653a470
Matt Plichta <bigcatplichta@gmail.com>
2020-01-09 22:34:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let multiply = num1 * num2;
 // return number between 0 and 100
 let random = Math.round(Math.random() * 100);
 
+let mod = 
+
 // apparently the tests don't want functions?
 
 // function multiply(num1, num2) {
